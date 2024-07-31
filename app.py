5f44acfb34dce42a6a43fbb147690f0fd012ee63
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-07-31 19:48:47 +0100

    Add HTML pages to app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1>Colm's Portfolio</h1>"
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# @app.route('/contact', method=['GET', 'POST'])
+
 
 if __name__ == "__main__":
     app.run()
