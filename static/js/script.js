d5499c1ecd976083a0b9166fd597d513f6c3fcda
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-08-10 18:47:53 +0100

    Added header, styling, JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript is working!');
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('JavaScript is working!');
+// });
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
